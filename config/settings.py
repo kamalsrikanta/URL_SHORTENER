41193aafaf223d79b4b6acbef7b8fdf1026cd07e
Srikanta kamal <kamalsrikanta77@gmail.com>
2026-06-08 22:48:23 +0530

    Fixed settings.py for deployment

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-   MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
